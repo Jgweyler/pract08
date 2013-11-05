1632c219a1379dba667373765f5701d5bd113365
GOMEZ WEYLER <alu4399@cc325.(none)>
2013-11-05 12:06:49 +0000

    Suma con errores

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -12,4 +12,18 @@ class Matrices
 		@fil = matriz.size	
 	end
 
+	def +(other)
+		i = 0
+                j = 0
+                sum = @mat
+                while i < @fil                   
+                   while j < @col
+                        sum[i,j] = sum[i,j] + other.mat[i,j]
+                        j += 1
+                   end
+                   i += 1
+                   j = 0
+                end
+                return sum
+        end
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -7,22 +7,36 @@ describe Matrices do
 	before :each do
 		@mat1 = Matrices.new([[0,0],[0,0]])
 		@mat2 = Matrices.new([[1,1],[1,1],[1,1]])
+		@mat3 = Matrices.new([[1,1],[1,1]])
+		@mat4 = Matrices.new([[2,2],[2,2]])
+		@mat5 = Matrices.new([[3,3],[3,3]])
 	end
 
-	it "Debe tener filas" do 
-		(defined?(@mat1.fil)).should == true
-		@mat1.fil.should eq(2)
-		(defined?(@mat2.fil)).should == true
-		@mat2.fil.should eq(3)
+	describe "# Creacion y almacenamiento de matriz" do
+		it "Debe tener filas" do 
+			(defined?(@mat1.fil)).should be_true
+			@mat1.fil.should eq(2)
+			(defined?(@mat2.fil)).should be_true
+			@mat2.fil.should eq(3)
 	
-	end
+		end
 
-	it "Debe tener columnas" do
-		(defined?(@mat1.col)).should == true
-		@mat1.col.should eq(2)
+		it "Debe tener columnas" do
+			(defined?(@mat1.col)).should be_true
+			@mat1.col.should eq(2)
 		
-		(defined?(@mat2.col)).should == true
-		@mat2.col.should eq(2)
+			(defined?(@mat2.col)).should be_true
+			@mat2.col.should eq(2)
+		end
+	end
+	
+	describe "# Operaciones aritmeticas" do
+		it "Debe realizarse la suma de matrices" do
+			(@mat3 + @mat4).should eq(@mat5)
+			#@resultado=@mat3+@mat4
+			#@resultado==@mat5
+			
+		end
 	end
 end
     
