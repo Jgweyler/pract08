ccff93b9bc20bf7284f820bf1bb0e3e4a480e2b9
Sara Báez García <alu0100693249@ull.edu.es>
2013-11-05 11:01:59 +0000

    Expectativas cumplidas para initialize y getters

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,1 +1,15 @@
-#fichero para el desarrolo de la clase matriz
+#Fichero para el desarrolo de la clase matriz
+
+require 'matrix'
+
+class Matrices
+	
+	attr_reader :mat, :col, :fil
+
+	def initialize(matriz)
+		@mat = matriz
+		@col = matriz[0].size 
+		@fil = matriz.size	
+	end
+
+end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,3 +1,28 @@
 #Fichero para definir las expectativas
 
 require "matriz.rb"
+
+describe Matrices do
+
+	before :each do
+		@mat1 = Matrices.new([[0,0],[0,0]])
+		@mat2 = Matrices.new([[1,1],[1,1],[1,1]])
+	end
+
+	it "Debe tener filas" do 
+		(defined?(@mat1.fil)).should == true
+		@mat1.fil.should eq(2)
+		(defined?(@mat2.fil)).should == true
+		@mat2.fil.should eq(3)
+	
+	end
+
+	it "Debe tener columnas" do
+		(defined?(@mat1.col)).should == true
+		@mat1.col.should eq(2)
+		
+		(defined?(@mat2.col)).should == true
+		@mat2.col.should eq(2)
+	end
+end
+    
